4c2f6b258cdc16bf69d311d275fdb4572f6242ff
Liar <1584481877@qq.com>
2022-02-16 16:11:11 +0800

    master commit1

---
+++ src/main/java/com/dinghanwen/git/gitTest.java
@@ -0,0 +1,11 @@
+package com.dinghanwen.git;
+
+/**
+ * @author Mr.Ding
+ * @create 2022-02-16 16:10
+ */
+public class gitTest {
+    public static void main(String[] args) {
+        System.out.println("hello,makeqing");
+    }
+}
